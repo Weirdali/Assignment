f9163e23efc70057e22423a79cd533d3e69ed8a6
alicegee <alicegee@ymail.com>
2016-11-23 16:39:25 +0000

    First Commit

---
+++ PHP/public_php/Asssignment/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alicegee
+ * Date: 23/11/2016
+ * Time: 13:52
+ */
+    ini_set('display_errors', 'On');
+    ini_set('html_errors', 'On');
+    ini_set('xdebug.trace_output_name', 'stock_quote.%t');
+    //	xdebug_start_trace();
+    include '../../includes/Assignment/bootstrap.php';
+    //	xdebug_stop_trace();
+?>
